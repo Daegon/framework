7365aa0537380380a23cc5fc7adb3f9830541fe0
zimm <Romsssa@gmail.com>
2014-10-27 13:03:40 +0200

    tmp fix "An uploaded file must be an array or an instance of UploadedFile"

--- src/Illuminate/Http/Request.php
+++ src/Illuminate/Http/Request.php
@@ -599,12 +599,16 @@ class Request extends SymfonyRequest {
 	{
 		if ($request instanceof static) return $request;
 
-		return (new static)->duplicate(
+        $files = $request->files->all();
+        foreach($files as $key => $file)
+            if($file === null)
+                $files[$key] = [];
+        return (new static)->duplicate(
 
-			$request->query->all(), $request->request->all(), $request->attributes->all(),
+            $request->query->all(), $request->request->all(), $request->attributes->all(),
 
-			$request->cookies->all(), $request->files->all(), $request->server->all()
-		);
+            $request->cookies->all(), $files, $request->server->all()
+        );
 	}
 
 	/**
